0a4fc060b914a16e51504efa8005ba5e7e4c6871
Will Donaldson <WillDonaldson@users.noreply.github.com>
2020-11-16 19:08:21 -0800

    LQR controller operational

--- simulation.py
+++ simulation.py
@@ -9,23 +9,43 @@ from time import time
 import variables
 
 fig = plt.figure()
-ax = fig.add_subplot(111, aspect = 'equal', xlim = (-15, 15), ylim = (-6, 6), title = "Inverted Pendulum Simulation")
+ax = fig.add_subplot(111, aspect = 'equal', xlim = (-5, 5), ylim = (-1, 1), title = "Inverted Pendulum Simulation")
 ax.grid()
 
 # animation parameters
 origin = [0.0, 0.0]
-dt = 0.05
+dt = 0.02
 frames = 200
 t_span = [0.0, frames * dt]
 
+# temporary storage, will restructure variables.py and stateSpace() later
+m1 = variables.m1
+m2 = variables.m2
+g = variables.g
+L = variables.L
+l = variables.l                  
+I = variables.I
+b = variables.b
+ss=np.array([0., 0., 0., 0.])
+
 pendulumArm = lines.Line2D(origin, origin, color='r') 
-cart = patches.Rectangle(origin, 1.5, 0.5, color='b')
-def stateSpace(t, initial):
+cart = patches.Rectangle(origin, 0.5, 0.15, color='b')
+
+def stateSpace(t, i):
     # returns state space model evaluated at initial conditions
-    ss = np.matmul(variables.A, np.transpose(initial))
+    S = np.sin(i[1])
+    C = np.cos(i[1])
+    denominator = (m2*l*C)**2 - (m1+m2) * (m2*(l**2) + I)
+    # nonlinear system
+    ss[0] = i[2]
+    ss[1] = i[3]
+    ss[2] = (1/denominator)*(-(m2**2)*g*(l**2)*C*S - m2*l*S*(m2*(l**2)+I)*(i[3]**2) + b*(m2*(l**2) + I)*i[2]) - ((m2*(l**2)+I)/denominator)*u(i)
+    ss[3] = (1/denominator)*((m1+m2)*m2*g*l*S + (m2**2)*(l**2)*S*C*(i[3]**2) - b*m2*l*C*i[2]) + ((m2*l*C)/denominator)*u(i)
     return ss
 
-initial=np.array([0, 7*np.pi/8, 0, 0])
+initial=np.array([-1, np.pi-0.5, -5, 0])      
+final=np.array([1, np.pi, 0, 0])
+u = lambda x: np.matmul(-variables.K, (x - final))
 ts = np.linspace(t_span[0], t_span[1], frames)
 pendulum_state = solve_ivp(stateSpace, t_span, initial, t_eval = ts, rtol=1e-8)
 
@@ -46,7 +66,7 @@ def animate(i):
     return pendulumArm, cart
 
 t0 = time()
-animate(0)                          #sample time required to evaluate function
+animate(0)                          # sample time required to evaluate animate() function
 t1 = time()
 interval = 1000 * dt - (t1 - t0)
 
--- variables.py
+++ variables.py
@@ -1,24 +1,48 @@
 import numpy as np
-
-m1 = 0.6
+import scipy.signal as signal
+import scipy.linalg as linalg
+m1 = 0.3
 m2 = 0.2
 g = 9.8
-L = 5
+L = 0.5
 l = L/2             # CoM of uniform rod                 
 I = (1./12) * m2 * (L**2)
-b = .2           # dampending coefficient
+b = 1           # dampending coefficient
 
-divisor = m1*m2*(l**2) + I*(m1 + m2)
-A21 = (m2**2)*g*(l**2) / divisor
-A31 = m2*g*l*(m1+m2) / divisor
-A22 = -b*(m2*(l**2)+I) / divisor
-A32 = -m2*l*b / divisor
-B2 = (m2*(l**2)+I) / divisor
-B3 = m2*l /divisor
+# linear system
+denominator = m1*m2*(l**2) + I*(m1 + m2)
+A21 = (m2**2)*g*(l**2) / denominator
+A31 = m2*g*l*(m1+m2) / denominator
+A22 = -b*(m2*(l**2)+I) / denominator
+A32 = -m2*l*b / denominator
+B2 = (m2*(l**2)+I) / denominator
+B3 = m2*l / denominator
 
-A = np.matrix([[0, 0, 1, 0],
-               [0, 0, 0, 1],
-               [0, A21, A22, 0],
-               [0, A31, A32, 0]])
+A = np.array([[0, 0, 1, 0],
+              [0, 0, 0, 1],
+              [0, A21, A22, 0],
+              [0, A31, A32, 0]])
 
-B = np.matrix([[0], [0], [B2], [B3]])
+B = np.array([[0], [0], [B2], [B3]])
+
+C = np.array([[1, 0, 0, 0],
+              [0, 1, 0, 0]])
+
+D = np.array([[0], [0]])
+
+Q = np.array([[1, 0, 0, 0],
+              [0, 10, 0, 0],
+              [0, 0, 1, 0],
+              [0, 0, 0, 1]])
+
+R = [[0.0001]]
+P = np.matrix(linalg.solve_continuous_are(A, B, Q, R))
+K = np.matrix(linalg.inv(R)*(B.T*P))
+eigVals, eigVecs = linalg.eig(A-B*K)
+
+print("eigVecs")
+print(eigVecs)
+print("eigVals")
+print(eigVals)
+print("K")
+print(K)
